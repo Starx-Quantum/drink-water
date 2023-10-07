e2824c081a25a6edf57e206c7479fb5443208de4
Akatsuki <138325648+akatsuki-9M@users.noreply.github.com>
2023-10-08 00:33:59 +0530

    Update script.js
    Changes and improvements:
    
    Added comments to explain the purpose of each section of the code.
    
    Defined maxLiters as a constant to make it clear that it represents the maximum liters when all cups are full.
    
    Used toFixed(2) to limit the number of decimal places for the liters display to two decimal places, which is a common way to display liters.
    
    Improved the code formatting for readability.

--- drink water/script.js
+++ drink water/script.js
@@ -1,49 +1,62 @@
-const smallCups = document.querySelectorAll('.cup-small')
-const liters = document.getElementById('liters')
-const percentage = document.getElementById('percentage')
-const remained = document.getElementById('remained')
+// Select all small cups, liters, percentage, and remained elements
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
 
-updateBigCup()
+// Initial function call to update the big cup
+updateBigCup();
 
+// Add click event listeners to each small cup
 smallCups.forEach((cup, idx) => {
-    cup.addEventListener('click', () => highlightCups(idx))
-})
+    cup.addEventListener('click', () => highlightCups(idx));
+});
 
+// Function to highlight cups based on the clicked index
 function highlightCups(idx) {
-    if (idx===7 && smallCups[idx].classList.contains("full")) idx--;
-    else if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
-        idx--
+    // Check if the last cup is full and prevent highlighting it
+    if (idx === 7 && smallCups[idx].classList.contains('full')) idx--;
+    else if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--;
     }
 
+    // Loop through all small cups to add or remove 'full' class
     smallCups.forEach((cup, idx2) => {
-        if(idx2 <= idx) {
-            cup.classList.add('full')
+        if (idx2 <= idx) {
+            cup.classList.add('full');
         } else {
-            cup.classList.remove('full')
+            cup.classList.remove('full');
         }
-    })
+    });
 
-    updateBigCup()
+    // Update the big cup
+    updateBigCup();
 }
 
+// Function to update the big cup and liters/percentage display
 function updateBigCup() {
-    const fullCups = document.querySelectorAll('.cup-small.full').length
-    const totalCups = smallCups.length
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+    const totalCups = smallCups.length;
+    const maxLiters = 2; // Maximum liters when all cups are full
 
-    if(fullCups === 0) {
-        percentage.style.visibility = 'hidden'
-        percentage.style.height = 0
+    if (fullCups === 0) {
+        // Hide percentage when there are no filled cups
+        percentage.style.visibility = 'hidden';
+        percentage.style.height = 0;
     } else {
-        percentage.style.visibility = 'visible'
-        percentage.style.height = `${fullCups / totalCups * 330}px`
-        percentage.innerText = `${fullCups / totalCups * 100}%`
+        // Show percentage and set its height based on filled cups
+        percentage.style.visibility = 'visible';
+        percentage.style.height = `${(fullCups / totalCups) * 330}px`; // Adjust as needed
+        percentage.innerText = `${(fullCups / totalCups) * 100}%`;
     }
 
-    if(fullCups === totalCups) {
-        remained.style.visibility = 'hidden'
-        remained.style.height = 0
+    if (fullCups === totalCups) {
+        // Hide remained when all cups are full
+        remained.style.visibility = 'hidden';
+        remained.style.height = 0;
     } else {
-        remained.style.visibility = 'visible'
-        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+        // Show remained liters based on filled cups
+        remained.style.visibility = 'visible';
+        liters.innerText = `${(maxLiters - (maxLiters * (fullCups / totalCups))).toFixed(2)}L`; // Adjust as needed
     }
 }
